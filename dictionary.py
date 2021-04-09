df4e81e8208a610ce948aa87906b51eaeacb75eb
Pablo Vasquez <pablo_vasquez@Pablos-MacBook-Pro.local>
2021-04-09 11:55:00 -0400

    I got help with this file so I could complete this lab.

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
+
+for k, v in myData.items():
+    print('key: ' , k, 'value: ' , v)
+
+print("ALL KEYS: " , myData.keys())
+print("ALL VALUES: " , myData.values())
