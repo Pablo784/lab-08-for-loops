28fe2a91d1f4463cad9f12ef8b266cdb4aee950d
Pablo Vasquez <pablo_vasquez@Pablos-MacBook-Pro.local>
2021-04-12 15:08:55 -0400

    Same file but edited

--- dictionary.py
+++ dictionary.py
@@ -1,10 +1,13 @@
 myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
-
+myKeys = []
+myValues = []
 for k, v in myData.items():
-    print('key: ' , k, 'value: ' , v)
+    print("key:"  + str(k) + ", value:" + str(v))
+    myKeys.append(k)
+    myValues.append(v)
 
-print("ALL KEYS: " , myData.keys())
-print("ALL VALUES: " , myData.values())
+print("ALL KEYS:" +str(myKeys))
+print("ALL VALUES:" +str(myValues))
 
-# In the terminal the ALL KEYS AND ALL VALUES HAS THE WORDS dict_keys and dict_values next to it
-# and I don't know why but in the end I got the ouput that I wanted.
+# I edited this filed since the professor decided to go over the file in class
+# on April 12th, 2021
